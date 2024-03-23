f56fca88ef509ae2244ee05c148e496d8e90aee6
apple-yagi <applesryu@gmail.com>
2024-03-24 00:24:32 +0900

    Update queries defaultOptions

--- workspaces/admin/src/lib/api/queryClient.ts
+++ workspaces/admin/src/lib/api/queryClient.ts
@@ -1,6 +1,13 @@
 import { MutationCache, QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
   mutationCache: new MutationCache({
     onError: (err) => {
       console.error(err);
