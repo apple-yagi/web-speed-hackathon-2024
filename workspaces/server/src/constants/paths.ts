bee89c09ee2f65b8df5fe3ab2b4bc9f92282de5e
apple-yagi <applesryu@gmail.com>
2024-03-23 19:17:39 +0900

    Fix to copy database.sqlite timming

--- workspaces/server/src/constants/paths.ts
+++ workspaces/server/src/constants/paths.ts
@@ -8,8 +8,6 @@ const PACKAGE_DIR = findPackageDir.sync()!;
 
 export const DATABASE_PATH = path.resolve(PACKAGE_DIR, './dist/database.sqlite');
 
-export const DATABASE_SEED_PATH = path.resolve(PACKAGE_DIR, './seeds/database.sqlite');
-
 export const IMAGES_PATH = path.resolve(PACKAGE_DIR, './dist/images');
 
 export const CLIENT_STATIC_PATH = path.resolve(WORKSPACE_DIR, './workspaces/client/dist');
