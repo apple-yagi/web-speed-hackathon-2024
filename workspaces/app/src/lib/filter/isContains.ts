564793bb09614454e966481e6ec0cd9270c48210
apple-yagi <applesryu@gmail.com>
2024-03-24 01:40:59 +0900

    Optimize isContains

--- workspaces/app/src/lib/filter/isContains.ts
+++ workspaces/app/src/lib/filter/isContains.ts
@@ -10,17 +10,22 @@ type Params = {
 
 // ひらがな・カタカナ・半角・全角を区別せずに文字列が含まれているかを調べる
 export function isContains({ query, target }: Params): boolean {
+  const queryLength = query.length;
+  const targetLength = target.length;
+
   // target の先頭から順に query が含まれているかを調べる
-  TARGET_LOOP: for (let offset = 0; offset <= target.length - query.length; offset++) {
-    for (let idx = 0; idx < query.length; idx++) {
+  for (let offset = 0; offset <= targetLength - queryLength; offset++) {
+    let found = true;
+    for (let idx = 0; idx < queryLength; idx++) {
       // 1文字ずつ Unicode Collation Algorithm で比較する
       // unicode-collation-algorithm2 は Default Unicode Collation Element Table (DUCET) を collation として使う
       if (compareWithFlags(target[offset + idx]!, query[idx]!, SENSITIVITY_ACCENT_FLAG) !== 0) {
-        continue TARGET_LOOP;
+        found = false;
+        break;
       }
     }
     // query のすべての文字が含まれていたら true を返す
-    return true;
+    if (found) return true;
   }
   // target の最後まで query が含まれていなかったら false を返す
   return false;
